93952196efe63bf6a51f9c6b9377fcde241cfa1c
Elena <hardevich@gmail.com>
2022-10-16 17:50:36 +0300

    hw01 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import './App.css'
-import { Message } from './Components/Message'
+import { Message } from './Components/Message/Message'
 
 function App() {
+  const [myName, setMyName] = useState('Elena')
+
+  const handleChangeName = (el) => {
+    setMyName(el.target.value)
+  }
   return (
     <div className="App">
-      <Message />
+      {' '}
+      Ниже отображается компонент Message
+      <Message name={myName} />
+      <p>Change name here</p>
+      <input onChange={handleChangeName} />
     </div>
   )
 }
